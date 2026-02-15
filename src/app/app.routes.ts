664552785e45947e98d9576ff2220829de130204
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-02-15 16:28:01 +0100

    Adding A "Not Found" Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes =[
     {
@@ -18,5 +19,8 @@ export const routes: Routes =[
                 path: 'tasks/new', component: NewTaskComponent
             }
         ]
+    },
+    {
+        path: '**', component: NotFoundComponent
     }
 ]
