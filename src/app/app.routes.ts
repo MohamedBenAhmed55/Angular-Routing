2a0a3e93b27ad668acac64fc036e9aab70de8495
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-02-15 16:33:16 +0100

    Redirecting Users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,9 @@ export const routes: Routes =[
         path: 'users/:userId', component: UserTasksComponent,
         children: [
             {
+                path: '', redirectTo: 'tasks', pathMatch: 'prefix'
+            },
+            {
                 path: 'tasks', component: TasksComponent
             },
             {
