983d5e953c05ac2233a39c611f02fc7f8d777e58
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-02-15 17:35:34 +0100

    Using Query Parameters For Data Manipulation

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,17 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
-import { ActivatedRoute, RouterLink } from "@angular/router";
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -12,21 +20,35 @@ import { ActivatedRoute, RouterLink } from "@angular/router";
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent implements OnInit{
+export class TasksComponent implements OnInit {
   userId = input.required<string>();
   // order = input<'asc' | 'desc'>();
-  order?: 'asc' | 'desc'
-  private tasksService = inject(TasksService)
-  userTasks = computed(() => this.tasksService.allTasks().filter(t => t.userId === this.userId()));
+  order = signal<'asc' | 'desc'>('desc');
+  private tasksService = inject(TasksService);
+  userTasks = computed(() =>
+    this.tasksService
+      .allTasks()
+      .filter((t) => t.userId === this.userId())
+      .sort((a,b) =>  {
+        if (this.order() === 'desc'){
+          return a.id > b.id ? -1 : 1
+        } else {
+          return a.id > b.id ? 1 : -1
+        }
+      }),
+  );
 
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParamMap.subscribe({
-      next: (params) => (this.order = params.get('order') as 'asc' | 'desc')
-    })
+      next: (params) => {
+        const order = params.get('order');
+        this.order.set(order === 'asc' ? 'asc' : 'desc');
+      },
+    });
 
-    this.destroyRef.onDestroy(() => subscription.unsubscribe)
+    this.destroyRef.onDestroy(() => subscription.unsubscribe);
   }
 }
